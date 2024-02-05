a6189a50b42a4d1a8e5ba5dfa8487854fd3f39f3
Ss Aaa <ssaaa.ele@gmail.com>
2024-02-05 13:58:58 +0100

    Algorithm good, rest to implement jacobsthal for the order of insertion

--- cpp05_09/cpp09/ex02/PmergeMe.cpp
+++ cpp05_09/cpp09/ex02/PmergeMe.cpp
@@ -52,48 +52,75 @@ void    algo_merge_list(std::list<int> &list)
     list = big;
 }
 
-void    insert_elem_vector(std::vector<int> &big, int elem)
+void    swap_range_vector(std::vector<int> &vector, int start1, int end1, int start2, int end2)
 {
-    for (size_t i = 0; i < big.size(); i++)
-        if (elem < big[i])
-        {
-            big.insert(big.begin() + i, elem);
-            return ;
-        }
-    big.push_back(elem);
+    std::vector<int> temp(vector.begin() + start2, vector.begin() + end2 + 1);
+    std::copy(vector.begin() + start1, vector.begin() + end1 + 1, vector.begin() + start2);
+    std::copy(temp.begin(), temp.end(), vector.begin() + start1);
 }
 
-void    sort_vector(std::vector<int> &big, std::vector<int> &little)
+void    dichotomy_vector(std::vector<int> &part_to_moov, std::vector<int> &vector, int begin, int end, int pair)
 {
-    for (size_t i = 0; i < little.size(); i++)
-        insert_elem_vector(big, little[i]);
+    int size = end - begin + 1;
+    if (size < 0 || size <= pair)
+        size = 0;
+    int good_index;
+    if ((size / pair) % 2 == 1)
+        good_index = begin + ((size - 1) / 2) + (pair / 2);
+    else
+        good_index = begin + (size / 2) + (pair - 1);
+    int number_moov = part_to_moov[part_to_moov.size() - 1];
+    int number_vector = vector[good_index];
+
+    if (size != 0)
+    {
+        if (number_moov <= number_vector)
+            dichotomy_vector(part_to_moov, vector, begin, good_index - pair, pair);
+        else
+            dichotomy_vector(part_to_moov, vector, good_index + 1, end, pair);
+    }
+    else
+    {
+        if (number_moov > number_vector)
+            begin += pair;
+        vector.insert(vector.begin() + begin, part_to_moov.begin(), part_to_moov.end());
+    }
+}
+
+void    sort_vector(std::vector<int> &vector, int range)
+{
+    int pair = range / 2;
+
+    for (size_t i = pair - 1; i < vector.size(); i += range)
+    {
+        std::vector<int> part_to_moov(vector.begin() + i - (pair - 1), vector.begin() + i + 1);
+        vector.erase(vector.begin() + i - (pair - 1), vector.begin() + i + 1);
+        dichotomy_vector(part_to_moov, vector, 0, i - (pair - 1) - 1, pair);
+    }
 }
 
 void    algo_merge_vector(std::vector<int> &vector)
 {
-    if (vector.size() < 2)
+    static int          range = 1;
+    std::vector<int>    impair;
+    range *= 2;
+
+    if (vector.size() / range < 1)
         return ;
 
-    std::vector<int>    little;
-    std::vector<int>    big;
-    int                 impair = -1;
-
-    if (vector.size() % 2 == 1)
+    if (vector.size() % range != 0)
     {
-        impair = vector[vector.size() - 1];
-        vector.pop_back();
+        impair.insert(impair.begin(), vector.end() - (vector.size() % range), vector.end());
+        vector.erase(vector.end() - (vector.size() % range), vector.end());
     }
 
-    for (size_t i = 0; i < vector.size(); i += 2)
-    {
-        big.push_back(std::max(vector[i], vector[i + 1]));
-        little.push_back(std::min(vector[i], vector[i + 1]));
-    }
-    algo_merge_vector(big);
-
-    sort_vector(big, little);
-    if (impair != -1)
-        insert_elem_vector(big, impair);
-
-    vector = big;
+    int pair = range / 2;
+    for (size_t i = pair - 1; i < vector.size(); i += range)
+        if (vector[i] > vector[i + pair])
+            swap_range_vector(vector, i - pair + 1, i, i + 1, i + pair);
+    algo_merge_vector(vector);
+    range /= 2;
+    sort_vector(vector, range);
+    if (impair.size() > 0)
+        dichotomy_vector(impair, vector, 0, vector.size() - 1, pair);
 }
